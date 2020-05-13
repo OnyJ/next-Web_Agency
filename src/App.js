a89a4072b189578a2d02648347cf2ba195d6126b
OnyJ <julien.josselin75@hotmail.fr>
2020-05-13 10:49:16 +0200

    install react-intl + create languages files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+// Router
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Works from "./pages/Works";
+// Components
 import Navbar from "./components/Navbar";
+// Language
+import { intlProvider } from "react-intl";
+import textFr from "./languages/fr";
+import textEn from "./languages/en";
+
+const texts = {
+  fr: textFr,
+  en: textEn,
+};
 
 function App() {
+  const [language, setLanguage] = useState("fr");
+
   return (
     <Router>
       <Navbar />
---
+++ src/languages/en/index.js
@@ -0,0 +1,11 @@
+import home from "./home.json";
+import about from "./about.json";
+import works from "./works.json";
+
+const en = {
+  ...home,
+  ...about,
+  ...works,
+};
+
+export default en;
